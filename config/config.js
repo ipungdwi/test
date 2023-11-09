a2680e1bdeae865645e1fc8597da3be96aa48d63
Dobberman <pangestubima89@gmail.com>
2023-11-09 12:42:48 +0700

    config

---
+++ config/config.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+module.exports = {
+    "development": {
+        "username": process.env.DB_USERNAME || "postgres",
+        "password": process.env.DB_PASSWORD || "secret",
+        "database": process.env.DB_NAME || "my_gram_development",
+        "host": process.env.DB_HOST || "127.0.0.1",
+        "dialect": process.env.DB_DIALECT || "postgres"
+    },
+    "test": {
+        "username": "root",
+        "password": null,
+        "database": "database_test",
+        "host": "127.0.0.1",
+        "dialect": "mysql"
+    },
+    "production": {
+        "username": "root",
+        "password": null,
+        "database": "database_production",
+        "host": "127.0.0.1",
+        "dialect": "mysql"
+    }
+}
