773b513eb32b24aef501d55f5a2fc0c4e9fb4f6b
Dobberman <pangestubima89@gmail.com>
2023-11-10 17:13:15 +0700

    refactor structure

--- controller/user.js
+++ controller/user.js
@@ -1,11 +1,20 @@
 const {User} = require("../models");
+class UserC {
+    static #hash;
+    static #compare_hash;
+    static #sign_token;
 
-module.exports = ({hash, compare_hash, sign_token}) => ({
-    register: async (req, res) => {
+    static Init({hash, compare_hash, sign_token}) {
+        this.#hash = hash;
+        this.#compare_hash = compare_hash;
+        this.#sign_token = sign_token;
+    }
+
+    static register = async (req, res) => {
         try {
             const {email, full_name, username, password, profile_image_url, age, phone_number} = req.body;
             const newUser = new User({
-                email, full_name, username, password: hash(password), profile_image_url, age, phone_number
+                email, full_name, username, password: this.#hash(password), profile_image_url, age, phone_number
             });
             await newUser.validate();
             await newUser.save();
@@ -17,7 +26,8 @@ module.exports = ({hash, compare_hash, sign_token}) => ({
         } catch (err) {
             res.sendStatus(500);
         }
-    }, login: async (req, res) => {
+    }
+    static login = async (req, res) => {
         try {
             const {email, password} = req.body;
             const newUser = await User.findOne({where: {email}});
@@ -26,18 +36,19 @@ module.exports = ({hash, compare_hash, sign_token}) => ({
                     code: 404, message: "user not found"
                 };
             }
-            if (!compare_hash(newUser.password, password)) {
+            if (!this.#compare_hash(newUser.password, password)) {
                 throw {
                     code: 400, message: "user password is wrong"
                 };
             }
             res.status(201).json({
-                token: sign_token({id: newUser.id})
+                token: this.#sign_token({id: newUser.id})
             });
         } catch (err) {
             res.sendStatus(err.code || 500);
         }
-    }, updateUser: async (req, res) => {
+    }
+    static updateUser = async (req, res) => {
         try {
             const {userId} = req.params;
             const {email, full_name, username, profile_image_url, age, phone_number} = req.body;
@@ -67,7 +78,8 @@ module.exports = ({hash, compare_hash, sign_token}) => ({
         } catch (err) {
             res.sendStatus(err.code || 500);
         }
-    }, deleteUser: async (req,res) => {
+    }
+    static deleteUser = async (req, res) => {
         try {
             const {userId} = req.params;
             if (parseInt(userId) !== req.user.id) {
@@ -84,8 +96,10 @@ module.exports = ({hash, compare_hash, sign_token}) => ({
             res.json({
                 message: "Your account has been successfully deleted"
             })
-        }catch (err) {
+        } catch (err) {
             res.sendStatus(err.code || 500);
         }
     }
-})
+}
+
+module.exports = UserC;
