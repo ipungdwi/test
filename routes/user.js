329a444eac04d7d9f4ae0a96e8d4c75ce5d70acf
Dobberman <pangestubima89@gmail.com>
2023-11-10 17:13:15 +0700

    refactor structure

--- routes/user.js
+++ routes/user.js
@@ -1,37 +1,6 @@
 const route = require("express").Router();
-const controller = require("../controller/user")({
-    hash: (password) => {
-        return password;
-    },
-    compare_hash: (cipher, password) => {
-        return true;
-    },
-    sign_token: (payload) => {
-        return payload.id;
-    }
-});
-const authMid = {
-    isAuth: async (req, res, next) => {
-        try {
-            const {token} = req.headers;
-            if (!token) {
-                throw {
-                    code: 401
-                };
-            }
-            req.user = await require("../models").User.findByPk(token, {});
-            console.log(req.user);
-            if (!req.user) {
-                throw {
-                    code: 401
-                };
-            }
-            next();
-        } catch (err) {
-            res.sendStatus(err.code || 500);
-        }
-    }
-}
+const controller = require("../controller/user");
+const authMid = require("../middleware/auth");
 
 route.post("/register", controller.register);
 route.post("/login", controller.login);
