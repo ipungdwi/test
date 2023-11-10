3643f6732686e2c93113c6889c1038f80c7022e0
Dobberman <pangestubima89@gmail.com>
2023-11-10 17:02:52 +0700

    create auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,24 @@
-module.exports = {
-    isAuth: (req,res,next)=>{}
+const {User} = require("../models");
+
+class AuthMid {
+    static #verify_token;
+
+    static Init({verify_token}) {
+        this.#verify_token = verify_token;
+    }
+
+    static isAuth = async (req, res, next) => {
+        const {token} = req.headers;
+        const claim = this.#verify_token(token);
+        if (!claim) {
+            return res.sendStatus(401);
+        }
+        req.user = await User.findByPk(claim.id);
+        if (!req.user) {
+            return res.sendStatus(401);
+        }
+        next();
+    }
 }
+
+module.exports = AuthMid;
