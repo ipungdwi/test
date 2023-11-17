aa56d2b539793406185b212d5929ebf8fe0f9f5e
ipungdwiari <c241dsx3663@bangkit.academy>
2023-11-17 11:09:51 +0700

    first commit

--- config/config.js
+++ config/config.js
@@ -15,10 +15,11 @@ module.exports = {
         "dialect": "mysql"
     },
     "production": {
-        "username": process.env.DB_USERNAME || "root",
-        "password": process.env.DB_PASSWORD || "secret",
-        "database": process.env.DB_NAME || "my_gram_production",
-        "host": process.env.DB_HOST || "127.0.0.1",
-        "dialect": process.env.DB_DIALECT || "mysql"
+        "username": process.env.DB_USERNAME_PROD || "root",
+        "password": process.env.DB_PASSWORD_PROD || "secret",
+        "database": process.env.DB_NAME_PROD || "my_gram_production",
+        "host": process.env.DB_HOST_PROD || "127.0.0.1",
+        "dialect": process.env.DB_DIALECT_PROD || "mysql",
+        "port": process.env.DB_PORT
     }
 }
